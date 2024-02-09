a5c9ef200b6cc78218b51a28988600a01090e656
iuricontarelli <1905iuri@gmail.com>
2024-02-09 16:21:23 -0300

    perguntas diferentes

--- script.js
+++ script.js
@@ -1,95 +1,96 @@
 const perguntas = [
-    {
-      pergunta: "Qual é o nome do mapa principal de League of Legends?",
-      respostas: [
-        "Summoner's Rift",
-        "Howling Abyss",
-        "Twisted Treeline"
-      ],
-      correta: 0 // Summoner's Rift
-    },
-    {
-      pergunta: "Qual é o nome do campeão que é conhecido como o Rei Destruído?",
-      respostas: [
-        "Darius",
-        "Garen",
-        "Mordekaiser"
-      ],
-      correta: 2 // Mordekaiser
-    },
-    {
-      pergunta: "Quantos jogadores compõem uma equipe padrão de League of Legends?",
-      respostas: [
-        "3",
-        "4",
-        "5"
-      ],
-      correta: 2 // 5
-    },
-    {
-      pergunta: "Qual é o nome do criador de League of Legends?",
-      respostas: [
-        "Jeff Kaplan",
-        "Brandon Beck",
-        "Marc Merrill"
-      ],
-      correta: 2 // Marc Merrill
-    },
-    {
-      pergunta: "Qual é o recurso principal usado para comprar itens durante uma partida?",
-      respostas: [
-        "Ouro",
-        "Cristais",
-        "Diamantes"
-      ],
-      correta: 0 // Ouro
-    },
-    {
-      pergunta: "Qual é o nome do objetivo neutro que concede bônus de poder ao time que o derrota?",
-      respostas: [
-        "Dragão",
-        "Arauto do Rift",
-        "Barão Nashor"
-      ],
-      correta: 2 // Barão Nashor
-    },
-    {
-      pergunta: "Qual é o nome da habilidade final de Yasuo?",
-      respostas: [
-        "Vento Cortante",
-        "Tornado",
-        "Último Suspiro"
-      ],
-      correta: 2 // Último Suspiro
-    },
-    {
-      pergunta: "Qual é o tipo de dano causado pelas habilidades de Lux?",
-      respostas: [
-        "Dano Físico",
-        "Dano Mágico",
-        "Dano Verdadeiro"
-      ],
-      correta: 1 // Dano Mágico
-    },
-    {
-      pergunta: "Qual é o nome da rota onde geralmente são encontrados os campeões lutadores?",
-      respostas: [
-        "Rota do Meio",
-        "Rota Superior",
-        "Rota Inferior"
-      ],
-      correta: 1 // Rota Superior
-    },
-    {
-      pergunta: "Qual é o nome do recurso usado para desbloquear novos campeões em League of Legends?",
-      respostas: [
-        "Pontos de Experiência",
-        "Essências Azuis",
-        "Pontos de Influência"
-      ],
-      correta: 1 // Essências Azuis
-    }
+  {
+    pergunta: "Qual é o maior planeta do sistema solar?",
+    respostas: [
+      "Júpiter",
+      "Saturno",
+      "Netuno"
+    ],
+    correta: 0 // Júpiter
+  },
+  {
+    pergunta: "Quem escreveu 'Dom Quixote'?",
+    respostas: [
+      "Miguel de Cervantes",
+      "William Shakespeare",
+      "Charles Dickens"
+    ],
+    correta: 0 // Miguel de Cervantes
+  },
+  {
+    pergunta: "Qual é o animal terrestre mais rápido?",
+    respostas: [
+      "Guepardo",
+      "Leopardo",
+      "Lobo Cinzento"
+    ],
+    correta: 0 // Guepardo
+  },
+  {
+    pergunta: "Qual é o maior oceano do mundo?",
+    respostas: [
+      "Pacífico",
+      "Atlântico",
+      "Índico"
+    ],
+    correta: 0 // Pacífico
+  },
+  {
+    pergunta: "Quem pintou a 'Mona Lisa'?",
+    respostas: [
+      "Leonardo da Vinci",
+      "Pablo Picasso",
+      "Vincent van Gogh"
+    ],
+    correta: 0 // Leonardo da Vinci
+  },
+  {
+    pergunta: "Quantos elementos químicos existem naturalmente?",
+    respostas: [
+      "118",
+      "92",
+      "100"
+    ],
+    correta: 1 // 92
+  },
+  {
+    pergunta: "Qual é a capital do Japão?",
+    respostas: [
+      "Tóquio",
+      "Pequim",
+      "Seul"
+    ],
+    correta: 0 // Tóquio
+  },
+  {
+    pergunta: "Quem foi o primeiro homem a pisar na lua?",
+    respostas: [
+      "Neil Armstrong",
+      "Buzz Aldrin",
+      "Yuri Gagarin"
+    ],
+    correta: 0 // Neil Armstrong
+  },
+  {
+    pergunta: "Qual é a montanha mais alta do mundo?",
+    respostas: [
+      "Monte Everest",
+      "Monte Kilimanjaro",
+      "Monte Fuji"
+    ],
+    correta: 0 // Monte Everest
+  },
+  {
+    pergunta: "Em que país está localizada a Grande Muralha?",
+    respostas: [
+      "China",
+      "Índia",
+      "Rússia"
+    ],
+    correta: 0 // China
+  }
 ];
+
 
 const quiz = document.querySelector('#quiz')
 const template = document.querySelector('template')
